800d39a156f914435e898d25fd9e4940b8706af4
Gabriel Balog <gabriel.balog@bradootech.com>
2018-07-09 20:30:48 -0300

    Adicionando o package react router Importando BrowserRouter e Route Escrevendo JSX para exibir algo Trabalhando com Route Escrevendo alguns jsx para serem chamados dentro do Router

--- src/app.js
+++ src/app.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 
+const ExpenseDashboardPage = () => (
+	<div>
+		This is from dashboard Page
+	</div>
+);
 
+const AddExpensePage = () => (
+	<div>
+		This is from add expense Page
+	</div>
+);
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+const EditExpensePage = () => (
+	<div>
+		This is from edit expense Page
+	</div>
+);
+
+const HelpPage = () => (
+	<div>
+		This is from help Page
+	</div>
+);
+
+const routes = (
+	<BrowserRouter>
+		<div>
+			<Route path="/" component={ExpenseDashboardPage} exact={true}/>
+			<Route path="/create" component={AddExpensePage}/>
+			<Route path="/edit" component={EditExpensePage}/>
+			<Route path="/help" component={HelpPage}/>
+		</div>
+	</BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
