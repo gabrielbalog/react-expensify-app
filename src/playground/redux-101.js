c354634560f861fe60803f6bfe98b88c22b5b05f
Gabriel Balog <gabriel.balog@bradootech.com>
2018-07-10 18:09:35 -0300

    Adicionando Actions para manusear data do store

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,7 +1,48 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }) => {
+const store = createStore((state = { count: 0 }, action) => {
+	switch (action.type) {
+		case 'INCREMENT':
+			return {
+				count: state.count + 1
+			}
+		case 'DECREMENT':
+			return {
+				count: state.count - 1
+			}
+		case 'RESET':
+			return {
+				count: 0
+			}
+		default:
+			return state;
+	}
 	return state;
+});
+
+console.log(store.getState());
+
+
+store.dispatch({
+	type: 'INCREMENT'
+});
+
+store.dispatch({
+	type: 'DECREMENT'
+});
+
+store.dispatch({
+	type: 'DECREMENT'
+});
+
+console.log(store.getState());
+store.dispatch({
+	type: 'RESET'
+});
+
+
+store.dispatch({
+	type: 'DECREMENT'
 });
 
 console.log(store.getState());
