d37c72df5156206f73f7778d1f2092711443932a
Gabriel Balog <gabriel.balog@bradootech.com>
2018-07-10 18:22:10 -0300

    Passando valores para manipulacao do store

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,16 +3,22 @@ import { createStore } from 'redux';
 const store = createStore((state = { count: 0 }, action) => {
 	switch (action.type) {
 		case 'INCREMENT':
+			const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
 			return {
-				count: state.count + 1
+				count: state.count + incrementBy
 			}
 		case 'DECREMENT':
+			const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
 			return {
-				count: state.count - 1
+				count: state.count - decrementBy
 			}
 		case 'RESET':
 			return {
 				count: 0
+			}
+		case 'SET':
+			return {
+				count: action.count
 			}
 		default:
 			return state;
@@ -20,29 +26,31 @@ const store = createStore((state = { count: 0 }, action) => {
 	return state;
 });
 
-console.log(store.getState());
-
+const unsubscribe = store.subscribe(() => {
+	console.log(store.getState());
+});
 
 store.dispatch({
-	type: 'INCREMENT'
+	type: 'INCREMENT',
+	incrementBy: 5
 });
+
+// unsubscribe(); // Para de printar as mudancas
 
 store.dispatch({
 	type: 'DECREMENT'
 });
 
 store.dispatch({
-	type: 'DECREMENT'
+	type: 'DECREMENT',
+	decrementBy: 5
 });
 
-console.log(store.getState());
 store.dispatch({
 	type: 'RESET'
 });
 
-
 store.dispatch({
-	type: 'DECREMENT'
+	type: 'SET',
+	count: 101
 });
-
-console.log(store.getState());
