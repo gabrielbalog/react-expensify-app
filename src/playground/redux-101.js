780c91cf3ac111388c7519bad23a0e79ce9c637b
Gabriel Balog <gabriel.balog@bradootech.com>
2018-07-11 18:06:03 -0300

    Adicionando Action Generators Removendo as chamadas antigas e utilizando as funcoes criadas

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,16 +1,40 @@
 import { createStore } from 'redux';
 
+// Action Generators
+
+// const add = ({ a, b }, c) => a + b + c;
+
+// console.log(add({ a:1, b: 12 }, 100));
+
+const incrementCount = ({ incrementBy = 1} = {}) => ({
+	type: 'INCREMENT',
+	incrementBy
+});
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+	'type': 'DECREMENT',
+	decrementBy
+});
+
+const setCount = ({ count = 0 } = {}) => ({
+	'type': 'SET',
+	count
+});
+
+
+const resetCount = () =>  ({
+	type: 'RESET'
+});
+
 const store = createStore((state = { count: 0 }, action) => {
 	switch (action.type) {
 		case 'INCREMENT':
-			const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
 			return {
-				count: state.count + incrementBy
+				count: state.count + action.incrementBy
 			}
 		case 'DECREMENT':
-			const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
 			return {
-				count: state.count - decrementBy
+				count: state.count - action.decrementBy
 			}
 		case 'RESET':
 			return {
@@ -30,27 +54,16 @@ const unsubscribe = store.subscribe(() => {
 	console.log(store.getState());
 });
 
-store.dispatch({
-	type: 'INCREMENT',
-	incrementBy: 5
-});
-
 // unsubscribe(); // Para de printar as mudancas
 
-store.dispatch({
-	type: 'DECREMENT'
-});
+store.dispatch(incrementCount({ incrementBy: 5 }));
+store.dispatch(incrementCount());
 
-store.dispatch({
-	type: 'DECREMENT',
-	decrementBy: 5
-});
 
-store.dispatch({
-	type: 'RESET'
-});
+store.dispatch(decrementCount());
+store.dispatch(decrementCount({ decrementBy: 10 }));
 
-store.dispatch({
-	type: 'SET',
-	count: 101
-});
+
+store.dispatch(resetCount());
+
+store.dispatch(setCount({ count: 5 }));
