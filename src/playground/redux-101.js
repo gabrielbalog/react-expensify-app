c3ed9621aab233988bdbddedf14dc34360a90920
Gabriel Balog <gabriel.balog@bradootech.com>
2018-07-11 18:16:31 -0300

    Movendo funcao anterior para um Reducer Criando novo arquivo para dados de Reducer

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -26,7 +26,9 @@ const resetCount = () =>  ({
 	type: 'RESET'
 });
 
-const store = createStore((state = { count: 0 }, action) => {
+// Reducers
+
+const countReducer = (state = { count: 0 }, action) => {
 	switch (action.type) {
 		case 'INCREMENT':
 			return {
@@ -47,8 +49,9 @@ const store = createStore((state = { count: 0 }, action) => {
 		default:
 			return state;
 	}
-	return state;
-});
+};
+
+const store = createStore(
 
 const unsubscribe = store.subscribe(() => {
 	console.log(store.getState());
