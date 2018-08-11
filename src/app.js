cc44fff7c899f0d7938cc2f6c9baa5248a166b1e
Gabriel Balog <gabriel.balog@bradootech.com>
2018-08-10 23:38:20 -0300

    Separando o codigo feito no playground para arquivos independentes Testando a inclusao e o filtro do Store no app.js Arrumando import e exports para que tudo funcione corretamente

--- src/app.js
+++ src/app.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter'
-import 'normalize.css/normalize.css'
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpense from './selectors/expenses';
+import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+const store = configureStore();
+
+store.dispatch(addExpense({ description: 'Water Bill'}));
+store.dispatch(addExpense({ description: 'Gas Bill'}));
+
+store.dispatch(setTextFilter('bill'));
+store.dispatch(setTextFilter('water'));
+
+const state = store.getState();
+const visibleExpense = getVisibleExpense(state.expenses, state.filters);
+console.log(visibleExpense);
+
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
