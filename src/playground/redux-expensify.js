feb21fc683c6af580ce34737e277285fb033f25c
Gabriel Balog <gabriel.balog@bradootech.com>
2018-08-10 22:33:40 -0300

    Adicionando funcao sortByAmount, adicionando no reducer e fazendo testes Adicionando funcao sortByDate, adicionando no reducer e fazendo testes Adicionando funcao setStartDate, adicionando no reducer e fazendo testes Adicionando funcao setEndDate, adicionando no reducer e fazendo testes

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -40,9 +40,26 @@ const setTextFilter = (text = '') => ({
 });
 
 // SORT_BY_DATE
+const sortByDate = () => ({
+	type: 'SORT_BY_DATE'
+});
+
 // SORT_BY_AMOUNT
+const sortByAmount = () => ({
+	type: 'SORT_BY_AMOUNT'
+});
+
 // SET_START_DATE
+const setStartDate = (startDate) => ({
+	type: 'SET_START_DATE',
+	startDate
+});
+
 // SET_END_DATE
+const setEndDate = (endDate) => ({
+	type: 'SET_END_DATE',
+	endDate
+});
 
 // Expenses Reducer
 
@@ -93,6 +110,26 @@ const filterReducer = (state = filterReducerDefaultState, action) => {
 				...state,
 				text: action.text
 			}
+		case 'SORT_BY_AMOUNT':
+			return {
+				...state,
+				sortBy: 'amount'
+			}
+		case 'SORT_BY_DATE':
+			return {
+				...state,
+				sortBy: 'date'
+			}
+		case 'SET_START_DATE':
+			return {
+				...state,
+				startDate: action.startDate
+			}
+		case 'SET_END_DATE':
+			return {
+				...state,
+				endDate: action.endDate
+			}
 		default:
 			return state;
 	}
@@ -115,9 +152,22 @@ const expenseOne = store.dispatch(addExpense({ description:'Rent', amount: 100 }
 const expenseTwo = store.dispatch(addExpense({ description:'Coffee', amount: 300 }));
 
 store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+
 store.dispatch(editExpense(expenseTwo.expense.id, { amount: 400 }));
+
 store.dispatch(setTextFilter('rent2'));
+
 store.dispatch(setTextFilter());
+
+store.dispatch(sortByAmount());
+
+store.dispatch(sortByDate());
+
+
+store.dispatch(setStartDate(125));
+store.dispatch(setStartDate());
+
+store.dispatch(setEndDate(1250));
 
 const demoState = {
 	expenses: [{
