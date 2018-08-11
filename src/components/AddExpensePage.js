2f2410b2e1479d105a1541495017f34c70e54b10
Gabriel Balog <gabriel.balog@bradootech.com>
2018-08-11 20:59:19 -0300

    Adicionando ExpenseForm e referenciando no AddExpensePage ExpenseForm possui um formulario que recebe description, note e amount Todos com funcao onchange para track do state Adicionado regex em amount, funciona parcialmente

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import ExpenseForm from './ExpenseForm';
 
 const AddExpensePage = () => (
 	<div>
-		This is from add expense Page
+		<h1>Add Expense</h1>
+		<ExpenseForm />
 	</div>
 );
 
