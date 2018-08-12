c7b068e073626064f61dc5e168bd61edc6a85798
Gabriel Balog <gabriel.balog@bradootech.com>
2018-08-11 21:40:14 -0300

    Adicionando validacao de preenchimento do formulario Adicionando funcao onSubmit no ExpenseForm e na AddExpensePage Adicionando no store a nova expense Redirecionando o usuario para a tela de dashboard ao adicionar a expense

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -14,7 +14,8 @@ export default class ExpenseForm extends React.Component {
 		note: '',
 		amount: '',
 		createdAt: moment(),
-		calendarFocused: false
+		calendarFocused: false,
+		error: ''
 	}
 	onDescriptionChange = (e) => {
 		const description = e.target.value;
@@ -27,20 +28,38 @@ export default class ExpenseForm extends React.Component {
 	onAmountChange = (e) => {
 		const amount = e.target.value;
 		// Me corrigir REGEX
-		if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+		if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
 			this.setState(() => ({ amount }));
 		}
 	}
 	onDataChange = (createdAt) => {
-		this.setState(() => ({  createdAt }));
+		if (createdAt) {
+			this.setState(() => ({  createdAt }));
+		}
 	}
 	onFocusChange = ({ focused }) => {
 		this.setState(() => ({  calendarFocused: focused }));
 	}
+	onSubmit = (e) => {
+		e.preventDefault();
+
+		if (!this.state.description || !this.state.amount) {
+			this.setState(() => ({ error: 'Please provide description and amount' }));
+		} else {
+			this.setState(() => ({ error: '' }));
+			this.props.onSubmit({
+				description: this.state.description,
+				amount: parseFloat(this.state.amount),
+				createdAt: this.state.createdAt.valueOf(),
+				note: this.state.note
+			})
+		}
+	}
 	render() {
 		return (
 			<div>
-			<form>
+			{this.state.error && <p>{this.state.error}</p>}
+			<form onSubmit={this.onSubmit}>
 				<input
 				type="text"
 				placeholder="Description"
@@ -49,8 +68,9 @@ export default class ExpenseForm extends React.Component {
 				onChange={this.onDescriptionChange}
 				/>
 				<input
-				type="number"
+				type="text"
 				placeholder="Amount"
+				onChange={this.onAmountChange}
 				/>
 				<SingleDatePicker 
 					date={this.state.createdAt}
