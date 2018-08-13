a3323dc3fc27f8862e5ec2956b3ce831f0d17353
Gabriel Balog <gabriel.balog@bradootech.com>
2018-08-13 18:20:19 -0300

    Adicionando os pacotes enzyme, enzyme-adapter-react-16, enzyme-to-json, raf no projeto Modificando o test de Header para utilizar o enzyme

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { shallow } from 'enzyme';
+import toJSON from 'enzyme-to-json';
 import ReactShallowRenderer from 'react-test-renderer/shallow';
 import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
-	const renderer = new ReactShallowRenderer();
-	renderer.render(<Header />);
-	expect(renderer.getRenderOutput()).toMatchSnapshot();
-})
+	const wrapper = shallow(<Header />);
+	expect(toJSON(wrapper)).toMatchSnapshot();
+	// expect(wrapper.find('h1').txt()).toBe('Expensify');
+	// const renderer = new ReactShallowRenderer();
+	// renderer.render(<Header />);
+	// expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
