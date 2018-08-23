d43725faaf0650e6ffde3785da198df50e7c7172
Gabriel Balog <gabriel.balog@bradootech.com>
2018-08-23 18:45:23 -0300

    Transformando o component EditExpensePage em class object Criando test para EditExpensePage

---
+++ src/tests/components/EditExpensePage.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import expenses from '../fixtures/expenses';
+import { EditExpensePage } from '../../components/EditExpensePage';
+
+let editExpense, removeExpense, history, wrapper;
+
+beforeEach(() => {
+	editExpense = jest.fn();
+	removeExpense = jest.fn();
+	history = { push: jest.fn() };
+	wrapper = shallow(
+		<EditExpensePage
+			editExpense={editExpense}
+			removeExpense={removeExpense}
+			history={history}
+			expense={expenses[2]}
+		/>
+	);
+});
+
+test('should render EditExpensePage', () => {
+	expect(wrapper).toMatchSnapshot();
+});
+
+test('should handle EditExpensePage', () => {
+	wrapper.find('ExpenseForm').prop('onSubmit')(expenses[2]);
+	expect(history.push).toHaveBeenCalledWith('/');
+	expect(editExpense).toHaveBeenCalledWith(expenses[2].id, expenses[2]);
+});
+
+test('should handle removeExpense', () => {
+	wrapper.find('button').simulate('click');
+	expect(history.push).toHaveBeenCalledWith('/');
+	expect(removeExpense).toHaveBeenCalledWith({
+		id: expenses[2].id
+	});
+});
