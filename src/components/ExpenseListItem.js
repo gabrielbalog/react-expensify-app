3c5b5d7484870423790c8f2dbdd5fdbc37c345b7
Gabriel Balog <gabriel.balog@bradootech.com>
2018-08-25 14:32:13 -0300

    Arrumando formataao de amount e createdAt e atualizando os snapshots de tests

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { removeExpense } from '../actions/expenses';
+import moment from 'moment';
+import numeral from 'numeral';
 
 export const ExpenseListItem = ({ id, description, amount, createdAt, dispatch }) => (
 	<div>
 		<Link to={`/edit/${id}`}>
 			<h3>{description}</h3>
 		</Link>
-		<p>{amount} - {createdAt}</p>
+		<p>
+		{numeral(amount / 100).format('$0,0.00')} 
+		- 
+		{moment(createdAt).format('MMMM Do, YYYY')}</p>
 	</div>
 );
 
