808d3f6bba485dd911f632952e6144a0ff67a6b6
Gabriel Balog <gabriel.balog@bradootech.com>
2018-08-27 19:52:34 -0300

    Adicionando action startSetExpenses, para receber os dados do firebase. Escrevendo os testes para capturar dados do firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -43,4 +43,19 @@ export const setExpenses = (expenses) => ({
   expenses
 });
 
-// export const startSetExpenses;
+export const startSetExpenses = () => {
+  return (dispatch) => {
+    return database.ref('expenses').once('value').then((snapshot) => {
+      const expenses = [];
+
+      snapshot.forEach((childSnapshot) => {
+        expenses.push({
+          id: childSnapshot.key,
+          ...childSnapshot.val()
+        });
+      });
+
+      dispatch(setExpenses(expenses));
+    });
+  };
+};
--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
+import { startSetExpenses } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpense from './selectors/expenses';
 import 'normalize.css/normalize.css';
@@ -17,6 +17,10 @@ const jsx = (
 	<Provider store={store}>
 		<AppRouter />
 	</Provider>
-)
+);
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+	ReactDOM.render(jsx, document.getElementById('app'));
+});
