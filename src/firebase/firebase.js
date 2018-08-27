e1969ec16bac3e31623973072ecf918e1d3fbf05
Gabriel Balog <gabriel.balog@bradootech.com>
2018-08-27 18:51:05 -0300

    Configurando o App para rodar em dois banco de dados diferentes. Um para producao e outro para tests

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyAE3js6gYdx7oLPutyPn3O7DOKSX88IJSE",
-    authDomain: "expensify-dcc51.firebaseapp.com",
-    databaseURL: "https://expensify-dcc51.firebaseio.com",
-    projectId: "expensify-dcc51",
-    storageBucket: "expensify-dcc51.appspot.com",
-    messagingSenderId: "405952206703"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABSE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 firebase.initializeApp(config);
 
