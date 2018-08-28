b0ca58c2f68ce767c6152131d48ffae485342b2d
Gabriel Balog <gabriel.balog@bradootech.com>
2018-08-28 19:14:59 -0300

    Adicionando botao para Deslogar Criando testes e adicionando nos componentes a opcao

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import toJSON from 'enzyme-to-json';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
-import Header from '../../components/Header';
+import { Header } from '../../components/Header';
 
 test('should render Header correctly', () => {
-	const wrapper = shallow(<Header />);
-	expect(toJSON(wrapper)).toMatchSnapshot();
+	const wrapper = shallow(<Header startLogout={() => { }}/>);
+	expect(wrapper).toMatchSnapshot();
+});
+
+test('should call startLogout on button click', () => {
+	const startLogout = jest.fn();
+	const wrapper = shallow(<Header startLogout={startLogout} />);
+	wrapper.find('button').simulate('click');
+	expect(startLogout).toHaveBeenCalled();
 });
