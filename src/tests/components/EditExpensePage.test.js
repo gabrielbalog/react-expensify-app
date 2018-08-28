c4daeb67ad9f08b08aec99aa6376cefa4c37b802
Gabriel Balog <gabriel.balog@bradootech.com>
2018-08-28 18:16:59 -0300

    Adicionando funcao startRemoveExpense, que substituira a removeExpense Testes e componentes atualizados para esta nova funcao

--- src/tests/components/EditExpensePage.test.js
+++ src/tests/components/EditExpensePage.test.js
@@ -3,16 +3,16 @@ import { shallow } from 'enzyme';
 import expenses from '../fixtures/expenses';
 import { EditExpensePage } from '../../components/EditExpensePage';
 
-let editExpense, removeExpense, history, wrapper;
+let editExpense, startRemoveExpense, history, wrapper;
 
 beforeEach(() => {
 	editExpense = jest.fn();
-	removeExpense = jest.fn();
+	startRemoveExpense = jest.fn();
 	history = { push: jest.fn() };
 	wrapper = shallow(
 		<EditExpensePage
 			editExpense={editExpense}
-			removeExpense={removeExpense}
+			startRemoveExpense={startRemoveExpense}
 			history={history}
 			expense={expenses[2]}
 		/>
@@ -29,10 +29,10 @@ test('should handle EditExpensePage', () => {
 	expect(editExpense).toHaveBeenCalledWith(expenses[2].id, expenses[2]);
 });
 
-test('should handle removeExpense', () => {
+test('should handle startRemoveExpense', () => {
 	wrapper.find('button').simulate('click');
 	expect(history.push).toHaveBeenCalledWith('/');
-	expect(removeExpense).toHaveBeenCalledWith({
+	expect(startRemoveExpense).toHaveBeenCalledWith({
 		id: expenses[2].id
 	});
 });
