3e9c466dd26ba9201be863db0ced95f60c5fd6fb
Gabriel Balog <gabriel.balog@bradootech.com>
2018-08-31 15:28:52 -0300

    Styling Expenses List Part II, terminando o style da lista de itens e da mensagem quando nao ha itens

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -7,11 +7,11 @@ import numeral from 'numeral';
 
 export const ExpenseListItem = ({ id, description, amount, createdAt, dispatch }) => (
 	<Link className="list-item" to={`/edit/${id}`}>
-	<div>
-		<h3>{description}</h3>
-		<span>{numeral(amount / 100).format('$0,0.00')} </span>
-	</div>
-	<h3>{moment(createdAt).format('MMMM Do, YYYY')}</h3>
+		<div>
+			<h3 className="list-item__title">{description}</h3>
+			<span className="list-item__sub-title">{moment(createdAt).format('MMMM Do, YYYY')}</span>
+		</div>
+		<h3 className="list-item__data">{numeral(amount / 100).format('$0,0.00')}</h3>
 	</Link>
 );
 
