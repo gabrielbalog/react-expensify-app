bba7365c84ae44f29748ef6d3c87e35e7cdfe97d
Gabriel Balog <gabriel.balog@bradootech.com>
2018-08-31 11:03:37 -0300

    Arrumando rota do update no firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -51,7 +51,7 @@ export const editExpense = (id, updates) => ({
 export const startEditExpense = (id, updates) => {
   return (dispatch, getState) => {
     const uid = getState().auth.uid;
-    return database.ref(`users/${uid}/expenses`).update(updates).then(() => {
+    return database.ref(`users/${uid}/expenses/${id}`).update(updates).then(() => {
       dispatch(editExpense(id, updates));
     });
   };
